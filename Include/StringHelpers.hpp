958a084e063e5dcc7ce00daec7e8960e6b4187c4
tungvh04 <tungvh04@gmail.com>
2023-11-21 22:00:34 +0700

    toString function

---
+++ Include/StringHelpers.hpp
@@ -0,0 +1,10 @@
+#ifndef STRINGHELPERS_HPP
+#define STRINGHELPERS_HPP
+
+#include <sstream>
+
+template <typename T>
+std::string toString(const T& value);
+
+#include <StringHelpers.inl>
+#endif // STRINGHELPERS_HPP
