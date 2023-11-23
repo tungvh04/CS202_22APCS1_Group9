7a44dbafd7f42fcb2cbe38c84f7c4086fc4e9501
tungvh04 <tungvh04@gmail.com>
2023-11-23 22:36:50 +0700

    define state and statestack

--- Include/ResourceIdentifiers.hpp
+++ Include/ResourceIdentifiers.hpp
@@ -3,6 +3,7 @@
 
 namespace sf {
     class Texture;
+    class Font;
 }
 
 namespace Textures {
@@ -12,9 +13,17 @@ namespace Textures {
     };
 }
 
+namespace Fonts {
+    enum ID {
+        Main,
+    };
+}
+
+// Forward declaration and a few type definitions
 template <typename Resource, typename Identifier>
 class ResourceHolder;
 
 typedef ResourceHolder<sf::Texture, Textures::ID> TextureHolder;
+typedef ResourceHolder<sf::Font, Fonts::ID> FontHolder;
 
 #endif // RESOURCEIDENTIFIERS_HPP
