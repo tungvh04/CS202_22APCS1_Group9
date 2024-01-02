39b8964406afe92c262f517bd4b98676c9f2cf72
Fidisk <70708171+Fidisk@users.noreply.github.com>
2024-01-02 21:39:18 +0700

    Initial Generate change
    Player first row should be grass, but the one behind can be whatever

--- Source/GameObject.cpp
+++ Source/GameObject.cpp
@@ -39,7 +39,18 @@ GameObject::GameObject(sf::Vector2f spawnOrigin, std::function<sf::FloatRect()> 
 }
 
 void GameObject::initialGenerate() {
-    for (int i = 0; i <= Constants::initialShift; i++) {
+    for (int i = 0; i < Constants::initialShift;i++) {
+        int rowType = rand() % RowObject::TypeCount;
+        SceneNode::Ptr tileRow(new TileRow(mTileTypes[rowType], getBattlefieldBounds, mTextures));
+        // SceneNode::Ptr obstacleRow(new ObstacleRow(sf::Vector2f(200, 0), mObstacleTypes[rowType], getBattlefieldBounds, mTextures));
+        SceneNode::Ptr obstacleRow(new ObstacleRow(mObstacleTypes[rowType], getBattlefieldBounds, mTextures));
+        tileRow.get()->setPosition(mSpawnOrigin);
+        obstacleRow.get()->setPosition(mSpawnOrigin);
+        mTiles->attachChild(std::move(tileRow));
+        mObstacles->attachChild(std::move(obstacleRow));
+        mSpawnOrigin.y -= Constants::GridSize;
+    }
+    for (int i = Constants::initialShift; i <= Constants::initialShift; i++) {
         SceneNode::Ptr tileRow(new TileRow(mTileTypes[RowObject::Grass], getBattlefieldBounds, mTextures));
         tileRow.get()->setPosition(mSpawnOrigin);
         mTiles->attachChild(std::move(tileRow));
