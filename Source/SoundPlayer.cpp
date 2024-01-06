29628cc21fa2bb1f41558ed477dade21c87d8265
Nhật Thanh <nhatthanhtran2606@gmail.com>
2024-01-06 18:36:57 +0700

    Change some sound and music, remove SoundNode

--- Source/SoundPlayer.cpp
+++ Source/SoundPlayer.cpp
@@ -19,7 +19,7 @@ SoundPlayer::SoundPlayer()
 : mSoundBuffers()
 , mSounds()
 {
-	mSoundBuffers.load(SoundEffects::Button,	"Media/Sound/Button.mp3");
+	mSoundBuffers.load(SoundEffects::Button, "Media/Sound/Button.mp3");
 
 	// Listener points towards the screen (default in SFML)
 	sf::Listener::setDirection(0.f, 0.f, -1.f);
